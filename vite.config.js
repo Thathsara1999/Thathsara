29e2637958a92914f35b09e96339d11fb2f10c1f
Nawanka Thathsara <141746388+Thathsara1999@users.noreply.github.com>
2025-06-11 10:30:25 +0530

    Refactor build config for Vercel and GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+const isGitHubPages = process.env.VITE_GH_PAGES === "true";
+
 export default defineConfig({
-  base: "/Thathsara/",
   plugins: [react()],
+  base: isGitHubPages ? "/My-Portfolio/" : "/",
   css: {
     modules: {
       localsConvention: "camelCase",
